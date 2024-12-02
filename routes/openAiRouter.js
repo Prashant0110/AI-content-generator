a7017fbfc897a54ef92152c8902ac51b5f51bc99
Prashant <prashantsapkota27@gmail.com>
2024-12-02 21:56:10 +0545

    Added apiLimit Middleware routes

--- routes/openAiRouter.js
+++ routes/openAiRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const checkAuthMiddleware = require("../middlewares/Authentication");
 const { openAIController } = require("../controllers/openAIController");
+const checkApiLimit = require("../middlewares/ApiLimit");
 const openAiRouter = express.Router();
 
-openAiRouter.post("/generate", checkAuthMiddleware, openAIController);
+openAiRouter.post(
+  "/generate",
+  checkAuthMiddleware,
+  checkApiLimit,
+  openAIController
+);
 
 module.exports = openAiRouter;
